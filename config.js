8f0325c1b6d1e49d8a4c842f2d61f3f3c45681b7
Valerii Pavlov <Valerii_Pavlov@epam.com>
2013-04-12 18:40:30 +0300

    Add parcing xml functionality

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = (function () {
+	var appPath = __dirname;
+	return {
+		pathSeparator : '\\',
+		path : {
+			xml : __dirname + '\\WebContent\\WEB-INF\\config\\sites\\diy'
+		}
+	}
+}());
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,60 @@
-
-/*
- * GET home page.
- */
-
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+	var fs = require('fs'),
+		cache = {},
+		parseString = require('xml2js').parseString,
+		xmlPath = require('../config').path.xml;
+		
+	function getPageConfig (path, callback) {
+		fs.readFile(path, "utf-8", function (err, data) {
+			if (err) return callback(err);
+			var separator = path.indexOf('/') !== -1 ? '/' : '\\',
+				chunks = path.split(separator),
+				name = chunks[chunks.length - 1];
+				
+			parseString(data, function (err, json) {
+				if (err) callback(err);
+				callback(null, {
+					name : name,
+					data : JSON.stringify(json)
+				});
+			})
+			
+		});
+	}
+	function getFilesList(dirPath, callback) {
+		//if (!fs) var fs = require('fs');
+		fs.readdir(dirPath, function (err, data) {
+			if (err) return callback(err, null);
+			callback(null, data);
+		});
+	}
+	function iterator(elems, handler, callback) {
+		var results = [];
+		(function recursor(items) {
+			if (!items.length) return callback(null, results);
+			var item = items.shift();
+			handler(item, function (err, data) {
+				if (err) callback(err, result);
+				results.push(data);
+				recursor(items);
+			});
+		})(elems);
+	}
+	function getFullFilesPath (files, prefix) {
+		var separator = require('../config').pathSeparator;
+		return files.map(function (fileName) {
+			return prefix + separator + fileName;
+		});
+	}
+	getFilesList(xmlPath, function (err, files) {
+		var filesList = getFullFilesPath(files, xmlPath);
+		
+		iterator(filesList, getPageConfig, function (err, data) {
+			res.render('index', {
+				title: 'Express',
+				pages : data
+			});
+		});
+	});
+	
 };
